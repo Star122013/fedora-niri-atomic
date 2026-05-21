7cb6896d85f70ebb29da2f8f1ac9d1c04dddeb48
cyrene <hyy122013@outlook.com>
2026-05-22 01:30:20 +0800

    update nixpkgs url

--- rootfs/etc/nix/flake.nix
+++ rootfs/etc/nix/flake.nix
@@ -6,14 +6,18 @@
     # Keep using a git-based flake input so revisions can still be locked.
     # SJTUG is preferred for binary cache in this repo, but does not currently document a nixpkgs.git mirror here,
     # so use NJU for flakes input fetching and keep other common mirrors as easy fallbacks.
-    nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixos-unstable&shallow=1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-unstable&shallow=1";
     # nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-unstable/nixexprs.tar.xz";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       # forAllSystems = f:
       #   builtins.listToAttrs (map (system: {
       #     name = system;
@@ -21,11 +25,14 @@
       #   }) systems);
       lib = nixpkgs.lib;
       forAllSystems = f: lib.genAttrs systems f;
-    in {
-      packages = forAllSystems (system:
+    in
+    {
+      packages = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
-        in {
+        in
+        {
           graphicsDrivers = pkgs.buildEnv {
             name = "system-nix-graphics-driver";
             paths = with pkgs; [
@@ -37,6 +44,7 @@
               intel-media-driver
             ];
           };
-        });
+        }
+      );
     };
 }
